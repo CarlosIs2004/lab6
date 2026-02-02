6fe65a7ba37c1bc26ef5aa4917d815ffe6d84d0a
CarlosIs2004 <carlos_izac8@hotmail.com>
2026-02-01 21:02:02 -0500

    Simulación de error en test

--- math.test.js
+++ math.test.js
@@ -2,9 +2,9 @@ const {fibonacci, factorial} = require('./math.js');
 
 
 test('Fibonacci de 6 debe ser 8', () => {
-    expect(fibonacci(6)).toBe(8);
+    expect(fibonacci(6)).toBe(8)
 });
 
 test('Factorial de 5 debe ser 120', () => {
-    expect(factorial(5)).toBe(120);
+    expect(factorial(5)).toBe(0);
 });
